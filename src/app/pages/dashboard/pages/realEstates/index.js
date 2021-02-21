d6dea0577509f1d871716290d33b43aad3ab7dc6
lfabl <met2net3@gmail.com>
2021-02-21 03:11:02 +0300

    * Emlak portföyüm  frontend tamamlandı.

--- src/app/pages/dashboard/pages/realEstates/index.js
+++ src/app/pages/dashboard/pages/realEstates/index.js
@@ -4,10 +4,22 @@ import React, {
 import injectSheet from 'react-jss';
 import stylesheet from './stylesheet';
 import {
+    TextInput,
     Button,
-    TextInput
+    Icon
 } from '../../../../components';
 import useGlobalState from '../../../../context';
+
+const STATES = {
+    "empty": {
+        "color": "orange",
+        "value": "Boş"
+    },
+    "active": {
+        "value": "Kullanımda",
+        "color": "green"
+    }
+};
 
 const RealEstates = ({
     classes
@@ -56,7 +68,45 @@ const RealEstates = ({
                 name: "plus-circle",
                 size: 22
             }}
+            className={classes.new}
         />
+        {
+            data && data.length ?
+                data.map((item, index) => {
+                    return <div
+                        key={index}
+                        className={classes.card}
+                        style={{
+                            backgroundColor: colors.background
+                        }}
+                    >
+                        <div>
+                            <div className={classes.cardTitle}>{item.title}</div>
+                            <div className={classes.cardInfo}>Durum: <span style={{
+                                color: STATES[item.state].color 
+                            }}>{STATES[item.state].value}</span></div>
+                            <div className={classes.cardInfo}>Kira Tarihi: <span>{item.date}</span></div>
+                            <div className={classes.cardInfo}>Kira Bedeli: <span>{item.amount}</span></div>
+                            <div className={classes.cardInfo}>Telefon Numarası: <span>{item.phone}</span></div>
+                            <div className={classes.cardInfo}>Mal Sahibi: <span>{item.malsahibisi}</span></div>
+                            <div className={classes.cardInfo}>Kiracı: <span>{item.kiraci}</span></div>
+                        </div>
+                        <div
+                            className={classes.cardLinkButton}
+                            onClick={() => {}}
+                        >
+                            <Icon
+                                name={item.state === "empty" ? "link" : "unlink"}
+                                size={26}
+                                color={STATES[item.state].color}
+                            />
+                            <span>{item.state === "empty" ? "Sözleşme Başlat" : "Sözleşmeyi Sonlandır"}</span>
+                        </div>
+                    </div>;
+                })
+                :
+                null
+        }
     </div>;
 };
 export default injectSheet(stylesheet)(RealEstates);
--- src/app/pages/dashboard/pages/realEstates/stylesheet.js
+++ src/app/pages/dashboard/pages/realEstates/stylesheet.js
@@ -1,4 +1,6 @@
 import {
+    active,
+    hover,
     objectSpacing 
 } from "../../../../theme/tokens";
 
@@ -13,6 +15,45 @@ const stylesheet = {
     },
     new: {
         marginBottom: objectSpacing * 3
+    },
+    card: {
+        marginBottom: objectSpacing * 1.5,
+        padding: objectSpacing * 2,
+        "&:hover": {
+            ...hover
+        },
+        "&:active": {
+            ...active
+        }
+    },
+    cardTitle: {
+        marginBottom: objectSpacing,
+        fontWeight: 600,
+        fontSize: 20
+    },
+    cardInfo: {
+        marginBottom: objectSpacing / 2,
+        fontWeight: 600,
+        "& span": {
+            fontWeight: 400
+        }
+    },
+    cardLinkButton: {
+        marginTop: objectSpacing,
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        position: "aboslute",
+        display: "flex",
+        "&:hover": {
+            ...hover
+        },
+        "&:active": {
+            ...active
+        },
+        "& span": {
+            marginLeft: objectSpacing
+        }
     }
 };
 export default stylesheet;
